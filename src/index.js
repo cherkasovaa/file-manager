6e3c84a1c6e2056237d170b351adbc1d76a26dc7
cherkasovaa <alina.ch1202@gmail.com>
2025-11-01 07:57:15 +0300

    feat: implement general functionality
    - Application accepts username and prints proper message
    - Application exits if user pressed ctrl+c or sent .exit command and proper message is printed

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import os from 'os';
+import process from 'process';
+import * as readline from 'readline';
+
+const args = process.argv.slice(2);
+const userNameArg = args.find(arg => arg.startsWith('--username='));
+const userName = userNameArg ? userNameArg.split('=')[1] : 'Anonymous'
+  
+let currentDir = os.homedir();
+  
+console.log(`Welcome to the File Manager, ${userName}!`)
+console.log(`You are currently in ${currentDir}!`)
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.on('line', (input) => {
+  const trimmedInput = input.trim();
+
+  if (trimmedInput === '.exit') {
+    rl.close();
+    return;
+  }
+
+  if (trimmedInput === 'ls') {
+    console.log('List files will be here...')
+  } else {
+    console.log('Invalid input')
+  }
+
+  console.log(`You are currently in ${currentDir}!`)
+})
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`)
+  process.exit(0)
+})
